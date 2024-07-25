a3bdb820d3fadda395b91e671daf96c9c44d10b9
diegonavarro1211 <diegonavarro@ufl.edu>
2024-07-25 15:17:19 -0400

    codes menu and functions

---
+++ Lab9.py
@@ -0,0 +1,28 @@
+def encode(password):
+    password_list=[]
+    for char in password:
+        new_num=int(char)
+        password_list.append(new_num)
+    return ''.join(password_list)
+
+def decode(password):
+    print(f"The encoded password is {encode(password)}, The original password is {password}")
+
+
+while True:
+    print("Menu")
+    print("------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        password = input("Please enter a password to encode: ")
+        encode(password)
+        print("Your password has been encoded and stored!")
+
+    elif option == 2:
+
+    elif option == 3:
+        print("")
+        break
